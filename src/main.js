11378868eb78a64a98a8d8d7fa529679e457b65a
Little-Karl <124084710+ltek0@users.noreply.github.com>
2023-04-26 12:46:58 +0800

    mobile and web "optmization"
    added animation sequence modification when small screen detected
    
    fixed banner background side in instead of fade in when using mobile size
    
    moved some css around
    
    added a noscript notification to the bottom

--- src/main.js
+++ src/main.js
@@ -13,4 +13,30 @@ function list() {
 $(document).ready(function() {
   $("#listdiv").load('/src/lists/ach.html');
   $('#serv-list').load('/src/lists/service-list.html');
+  mobi_css();
 });
+
+function mobi_css(){
+  var home_b1 = document.getElementById("home_b1")
+  var about_b1 = document.getElementById("about_b1")
+  var about_b2 = document.getElementById("about_b2")
+  var about_b3 = document.getElementById("about_b3")
+
+  if( window.innerWidth <= 575) {
+    console.log("mobile?");
+    if (home_b1){home_b1.style.animationDelay='.9s';}
+    else if(about_b1 || about_b2 || about_b3){
+      about_b1.style.animationDelay='.9s';
+      about_b2.style.animationDelay='1.1s';
+      about_b3.style.animationDelay='1.3s';
+    }
+  } else {
+    if (home_b1){home_b1.style.animationDelay='.4s';}
+    else if(about_b1 || about_b2 || about_b3){
+      about_b1.style.animationDelay='.4s';
+      about_b2.style.animationDelay='.8s';
+      about_b3.style.animationDelay='.9s';
+    }
+  }
+
+}
